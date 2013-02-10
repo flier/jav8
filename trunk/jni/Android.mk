350157c911a9362d025e5e2ddc7ce564c3bd2be0
flier.lu@gmail.com <flier.lu@gmail.com@d988de57-74f3-0aac-b178-8553cf931c56>
2013-02-10 11:48:38 +0000

    add android supports with Maven

--- trunk/jni/Android.mk
+++ trunk/jni/Android.mk
@@ -1,13 +1,16 @@
+# Variable default definitions. Override them by exporting them in your shell.
+V8_HOME				?=
+
 LOCAL_PATH := $(call my-dir)
 
-ANDROID_SOURCE		:= D:/Mobile/android-source
-V8_HOME 			:= $(ANDROID_SOURCE)/external/v8
+include $(CLEAR_VARS)
+LOCAL_MODULE    	:= v8-base-prebuilt
+LOCAL_SRC_FILES		:= $(TARGET_ARCH_ABI)/libv8_base.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE    	:= v8-prebuilt
-LOCAL_SRC_FILES		:= $(TARGET_ARCH_ABI)/libv8.a
-
+LOCAL_MODULE    	:= v8-snapshot-prebuilt
+LOCAL_SRC_FILES		:= $(TARGET_ARCH_ABI)/libv8_snapshot.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -15,21 +18,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    	:= jav8
 LOCAL_SRC_FILES 	:= jav8.cpp Wrapper.cpp Utils.cpp
 
-LOCAL_CPP_FEATURES 	:= #rtti #exceptions
+LOCAL_CPP_FEATURES 	:= rtti exceptions
 LOCAL_CFLAGS += \
-	-Wno-endif-labels \
-	-Wno-import \
-	-Wno-format \
-	-fno-exceptions \
 	-DENABLE_DEBUGGER_SUPPORT \
 	-DV8_NATIVE_REGEXP \
-	-fvisibility=hidden \
 	-DARM \
 	-DV8_TARGET_ARCH_ARM \
 	-DENABLE_LOGGING_AND_PROFILING
-	
-LOCAL_C_INCLUDES 		:= $(V8_HOME) $(V8_HOME)/include
-LOCAL_STATIC_LIBRARIES 	:= v8-prebuilt
+
+LOCAL_C_INCLUDES 		:= $(V8_HOME) $(V8_HOME)/include $(V8_HOME)/src
+LOCAL_STATIC_LIBRARIES 	:= v8-base-prebuilt v8-snapshot-prebuilt
 LOCAL_LDLIBS 			:= -llog
 
 include $(BUILD_SHARED_LIBRARY)

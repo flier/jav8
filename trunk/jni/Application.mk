d123b79030ea0051ed59c0373f278335fd3dbabf
flier.lu@gmail.com <flier.lu@gmail.com@d988de57-74f3-0aac-b178-8553cf931c56>
2013-02-12 12:50:59 +0000

    add javax.script package for android

--- trunk/jni/Application.mk
+++ trunk/jni/Application.mk
@@ -1,3 +1,4 @@
-APP_STL 	:= stlport_static
-APP_ABI		:= armeabi
-APP_GNUSTL_FORCE_CPP_FEATURES := exceptions rtti
+APP_STL 						:= stlport_static
+APP_ABI							:= armeabi
+APP_GNUSTL_FORCE_CPP_FEATURES 	:= exceptions rtti
+APP_PLATFORM 					:= android-3
